79e3f2b288d125e804331d9fcd4234d4afd754ca
aondrej ryan <aondrejryan11@gmail.com>
2024-02-20 15:58:10 -0800

    idk what im doing

---
+++ client/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  const [array, setArray] = React.useState<any>([])
+  return (
+    <div>lol</div>
+  );
+}
+
+export default App;

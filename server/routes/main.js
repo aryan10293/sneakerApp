d5731dab1d22424e5ca83bc42686d0bfa9c410d0
aondrej ryan <aondrejryan11@gmail.com>
2024-03-15 11:35:00 -0700

    idk whats going on

--- server/routes/main.js
+++ server/routes/main.js
@@ -1,7 +1,6 @@
 import express from "express";
 const router = express.Router();
 import auth from "../controller/auth.js"
-import workout from "../controller/workout.js";
 import authenticateToken from "../middleware/jwt.js";
 
 
@@ -9,6 +8,5 @@ import authenticateToken from "../middleware/jwt.js";
 router.post('/createaccount', auth.postCreateAccount)
 router.post('/login', auth.postLogin)
 router.use(authenticateToken);
-router.post('/workoutplan', workout.workoutPlan)
 router.get('/getuser/:token',  auth.checkUser)
 export default router;
